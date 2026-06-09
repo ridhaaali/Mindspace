582cd6248c1781ef0740f1805f1ad2d1f8cff96e
Ali Ridhaa <ridhaaali7@gmail.com>
2026-06-09 15:22:29 +0530

    Add state management to login form

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,39 +1,47 @@
+"use client";
+import { useState } from "react";
 import Link from "next/link";
 export default function LoginPage() {
-  return(
-    <div className="min-h-screen flex items-center justify-center">
-        <div className="w-full max-w-md p-8 border rounded-xl">
-            <h1 className="text-3xl font-bold text-center">
-                Welcome Back
-            </h1>
-            <form>
-                <input
-                    type="email"
-                    placeholder="Enter your email id"
-                    className="w-full mt-6 p-3 border"
-                />
-                <input
-                    type="password"
-                    placeholder="Enter your password"
-                    className="w-full mt-4 p-3 border"
-                />
-                <div className="flex justify-center mt-6">
-                    <button className=" bg-fuchsia-200 text-black px-6 py-3 border border-b-black rounded-xl hover:bg-fuchsia-100">
-                        Login
-                    </button>
+    const [email, setEmail] = useState("");
+    const [password, setPassword]=useState("");
+    console.log(email);
+    console.log(password);
+    return(
+        <div className="min-h-screen flex items-center justify-center">
+            <div className="w-full max-w-md p-8 border rounded-xl">
+                <h1 className="text-3xl font-bold text-center">
+                    Welcome Back
+                </h1>
+                <form>
+                    <input
+                        type="email"
+                        placeholder="Enter your email id"
+                        className="w-full mt-6 p-3 border"
+                        onChange={(event) => setEmail(event.target.value)}
+                    />
+                    <input
+                        type="password"
+                        placeholder="Enter your password"
+                        className="w-full mt-4 p-3 border"
+                        onChange={(event) => setPassword(event.target.value)}
+                    />
+                    <div className="flex justify-center mt-6">
+                        <button type="submit" className=" bg-fuchsia-200 text-black px-6 py-3 border border-b-black rounded-xl hover:bg-fuchsia-100">
+                            Login
+                        </button>
+                    </div>
+                </form>
+                <div className="mt-6 text-center">
+                    <p>
+                        Don&apos;t have an account?{" "}
+                        <Link
+                            href="/signup"
+                            className="text-purple-300 hover:text-purple-200">
+                                Sign Up
+                        </Link>
+                    </p>
                 </div>
-            </form>
-            <div className="mt-6 text-center">
-                <p>
-                    Don&apos;t have an account?{" "}
-                    <Link
-                        href="/signup"
-                        className="text-purple-300 hover:text-purple-200">
-                            Sign Up
-                    </Link>
-                </p>
             </div>
         </div>
-    </div>
-  );
+    );
 }
